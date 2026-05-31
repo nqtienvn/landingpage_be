78bbd9fec035a6d3f47fbeb322cbe356374e511d
nqtienvn <quangtiencoder1876@gmail.com>
2026-05-31 15:51:42 +0700

    chore: configure Redis serializer to support Java 8 Date/Time types

--- src/main/java/vn/com/be_landingpage/config/CacheConfig.java
+++ src/main/java/vn/com/be_landingpage/config/CacheConfig.java
@@ -1,6 +1,8 @@
 package vn.com.be_landingpage.config;
 
 import java.time.Duration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +15,19 @@ public class CacheConfig {
 
     @Bean
     RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY
+        );
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
         return builder -> builder.cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new GenericJackson2JsonRedisSerializer()
-                )));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)));
     }
 }
